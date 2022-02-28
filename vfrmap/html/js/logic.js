9631db95f752a61281bfe2c32a2cf298cee350a7
Nicolas S. Ballada <nballada@Nicolas-Mac-Mini.local>
2022-02-28 00:03:14 -0500

    bugfixes

--- vfrmap/html/js/logic.js
+++ vfrmap/html/js/logic.js
@@ -1,13 +1,14 @@
+
 let vatMap = () => {
   let url = `https://map.vatsim.net`;
-  window.open(url, '_blank')
+  window.open(url, '_blank');
 }
 
 let airNav = (_icao) => {
   let icao = _icao || 'ABCD';
   let err = document.getElementById('airerr');
-  if (icao !== '' || icao !== undefined) {
-    let url = `https://www.airnav.com/airport/${icao}`
+  if (icao !== '' || icao !== undefined || icao !== 'ABCD') {
+    let url = `https://www.airnav.com/airport/${icao}`;
     window.open(url, '_blank');
     err.innerHTML = '';
   } else {
@@ -19,8 +20,9 @@ let airNav = (_icao) => {
 let metar = (_icao) => {
   let icao = _icao || 'ABCD';
   let err = document.getElementById('meterr');
-  if (icao !== '' || icao !== undefined) {
-    let url = `https://www.aviationweather.gov/adds/metars/index?submit=1&station_ids=${icao}&chk_metars=on&hoursStr=8&std_trans=translated`
+  console.log(icao);
+  if (icao !== '' || icao !== undefined || icao !== 'ABCD') {
+    let url = `https://www.aviationweather.gov/adds/metars/index?submit=1&station_ids=${icao}&chk_metars=on&hoursStr=8&std_trans=translated`;
     window.open(url, '_blank');
     err.innerHTML = '';
   } else {
@@ -32,8 +34,8 @@ let metar = (_icao) => {
 let skyVector = (_icao) => {
   let icao = _icao || 'ABCD';
   let err = document.getElementById('skyerr');
-  if (icao !== '' || icao !== undefined) {
-    let url = `https://skyvector.com/airport/${icao}`
+  if (icao !== '' || icao !== undefined || icao !== 'ABCD') {
+    let url = `https://skyvector.com/airport/${icao}`;
     window.open(url, '_blank');
     err.innerHTML = '';
   } else {
@@ -42,20 +44,47 @@ let skyVector = (_icao) => {
   }
 }
 
-let clearAll = () => {
-  let meterr = document.getElementById('meterr');
-  let airerr = document.getElementById('airerr');
-  let skyerr = document.getElementById('skyerr');
-  let metText = document.getElementById('metText');
-  let airText = document.getElementById('airText');
-  let skyText = document.getElementById('skyText');
+let syncServices = () => {
+  let metText = document.getElementById('metText').innerHTML ? document.getElementById('metText').innerHTML : undefined;
+  let airText = document.getElementById('airText').innerHTML ? document.getElementById('airText').innerHTML : undefined;
+  let skyText = document.getElementById('skyText').innerHTML ? document.getElementById('skyText').innerHTML : undefined;
 
-  meterr.innerHTML = '';
-  airerr.innerHTML = '';
-  skyerr.innerHTML = '';
-  metText.innerHTML = '';
-  airText.innerHTML = '';
-  skyText.innerHTML = '';
+  if (metText && airText && metText === airText && skyText !== metText) {
+    skyText = metText;
+  } else if (metText && skyText && metText === skyText && airText !== metText) {
+    airText = metText;
+  } else if (airText && skyText && airText === skyText && metText !== airText) {
+    metText = airText;
+  } else {
+    if (skyText && !metText && !airText) {
+      metText = skyText;
+      airText = skyText;
+    } else if (airText && !metText && !skyText) {
+      metText = airText;
+      skyText = airText;
+    } else if (metText && !airText && !skyText) {
+      airText = metText;
+      skyText = metText;
+    } else {
+      console.log(`cannot sync, differing entries met ${metText} | sky ${skyText} | air ${airText}`);
+    }
+  }
+}
+
+let clearAll = () => {
+  let meterr = document.getElementById('meterr').innerHTML ? document.getElementById('meterr').innerHTML : '';
+  let airerr = document.getElementById('airerr').innerHTML ? document.getElementById('airerr').innerHTML : '';
+  let skyerr = document.getElementById('skyerr').innerHTML ? document.getElementById('skyerr').innerHTML : '';
+  let metText = document.getElementById('metText').innerHTML ? document.getElementById('metText').innerHTML : '';
+  let airText = document.getElementById('airText').innerHTML ? document.getElementById('airText').innerHTML : '';
+  let skyText = document.getElementById('skyText').innerHTML ? document.getElementById('skyText').innerHTML : '';
+
+
+  airerr = '';
+  skyerr = '';
+  metText = '';
+  airText = '';
+  skyText = '';
 }
 
 let expandList = (_icaoText) => {
@@ -68,10 +97,10 @@ let expandList = (_icaoText) => {
   }
 }
 
-let open = (_service) => {
-  let meterr = document.getElementById('meterr').innerHTML;
-  let airerr = document.getElementById('airerr').innerHTML;
-  let skyerr = document.getElementById('skyerr').innerHTML;
+let openService = (_service) => {
+  let meterr = document.getElementById('meterr');
+  let airerr = document.getElementById('airerr');
+  let skyerr = document.getElementById('skyerr');
   let metarList = document.getElementById('metText').value || undefined;
   let airNavList = document.getElementById('airText').value || undefined;
   let skyVectorList = document.getElementById('skyText').value || undefined;
@@ -86,7 +115,7 @@ let open = (_service) => {
           metar(icao);
         });
       } else {
-        meterr = `metar empty | ${metarList} | ${service}`;
+        meterr.innerHTML = `metar empty | ${metarList} | ${service}`;
       }
       break;
     case 'airnav':
@@ -96,7 +125,7 @@ let open = (_service) => {
           airNav(icao);
         });
       } else {
-        airerr = `airNav empty | ${airNavList} | ${service}`;
+        airerr.innerHTML = `airNav empty | ${airNavList} | ${service}`;
       }
       break;
     case 'skyvector':
@@ -106,7 +135,7 @@ let open = (_service) => {
           skyVector(icao);
         });
       } else {
-        skyerr = `skyVector empty | ${skyVectorList} | ${service}`;
+        skyerr.innerHTML = `skyVector empty | ${skyVectorList} | ${service}`;
       }
       break;
     case 'all':
@@ -116,7 +145,7 @@ let open = (_service) => {
           metar(icao);
         });
       } else {
-        meterr = `metar empty | ${metarList} | ${service}`;
+        meterr.innerHTML = `metar empty | ${metarList} | ${service}`;
       }
       if (airNavList) {
         icaoList = expandList(airNavList);
@@ -124,7 +153,7 @@ let open = (_service) => {
           airNav(icao);
         });
       } else {
-        airerr = `airNav empty | ${airNavList} | ${service}`;
+        airerr.innerHTML = `airNav empty | ${airNavList} | ${service}`;
       }
       if (skyVectorList) {
         icaoList = expandList(skyVectorList);
@@ -132,7 +161,7 @@ let open = (_service) => {
           skyVector(icao);
         });
       } else {
-        skyerr = `skyVector empty | ${skyVectorList} | ${service}`;
+        skyerr.innerHTML = `skyVector empty | ${skyVectorList} | ${service}`;
       }
       break;
     default:
